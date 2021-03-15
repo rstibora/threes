ed7baafaf936cd225355ac88f0b6cc5c14d8ffcf
Radek Stibora <rstibora@gmail.com>
2021-03-15 18:41:39 +0100

    Add custom user admin for EmailUser

--- backend/threes/apps/core/admin.py
+++ backend/threes/apps/core/admin.py
@@ -4,4 +4,19 @@ from django.contrib.auth.admin import UserAdmin
 from .models import EmailUser
 
 
-admin.site.register(EmailUser, UserAdmin)
+class EmailUserAdmin(UserAdmin):
+    model = EmailUser
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")})
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "field": ("username", "password1", "password2"),
+        }),
+    )
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("email", "is_staff", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
--- backend/threes/apps/core/models.py
+++ backend/threes/apps/core/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class EmailUser(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), unique=True)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return self.email
