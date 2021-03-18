03135c70c44220137b981e75307d3254d540d277
Radek Stibora <rstibora@gmail.com>
2021-03-18 18:00:09 +0100

    Enable css loading in single file components

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -27,6 +27,13 @@ module.exports = {
             { 
                 test: /\.vue$/,
                 loader: "vue-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "vue-style-loader",
+                    "css-loader",
+                ]
             }
         ]
     },
