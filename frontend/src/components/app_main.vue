159c6b79bbfbeb4423df72f03d785c6dfa6bc3cf
Radek Stibora <rstibora@gmail.com>
2021-03-18 18:49:59 +0100

    Somehow create a sidebar

--- frontend/src/components/app_main.vue
+++ frontend/src/components/app_main.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="yololo">{{ msg }}</div>
+<div class="app-inner">
+    <aside class="navbar">
+    </aside>
+
+    <div class="content"></div>
+</div>
 </template>
 
 <script lang="ts">
@@ -13,3 +18,36 @@ export default defineComponent({
     }
 })
 </script>
+
+<style>
+html, body {
+    height: 100%;
+    min-height: 100%;
+}
+
+#app {
+    display: flex;
+    align-items: stretch;
+    min-height: 100%;
+    max-height: 100%;
+    width: 100%;
+}
+
+.app-inner {
+    display: flex;
+    flex-direction: row;
+    min-height: 100%;
+    max-height: 100%;
+    width: 100%;
+}
+
+.navbar {
+    width: 80px;
+    height: 100%;
+
+    background-color: red;
+}
+.content {
+    background-color: blue;
+}
+</style>
