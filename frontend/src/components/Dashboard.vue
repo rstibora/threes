4173a9807af89b19b95708c55a1f17d359d96842
Radek Stibora <rstibora@gmail.com>
2021-03-19 20:15:10 +0100

    Add initial version of Task component

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,17 +1,40 @@
 <template>
-    <div class="columns">
-        <div class="column">
-            DASHBOARD
-        </div>
+<div class="vertical-split-container">
+    <div class="left-split">
+        <task v-for="item in items" :key="item"/>
     </div>
+    <div class="right-split">
+    </div>
+
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue"
+
+import Task from "./tasks/Task.vue"
 
 export default defineComponent({
-    setup() {
-        
+    data: function() {
+        return {
+            items: [1, 2, 3]
+        }
     },
+    components: {
+        Task,
+    }
 })
 </script>
+
+<style scoped>
+.vertical-split-container {
+    display: flex;
+
+    height: 100%;
+    min-height: 100%;
+}
+.left-split, .right-split {
+    display: flex;
+    flex-direction: column;
+}
+</style>
