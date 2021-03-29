44501db4bb6e30e5442822f2af16f68684533e75
Radek Stibora <rstibora@gmail.com>
2021-03-29 20:13:30 +0200

    Add initial version of Effort model

---
+++ backend/threes/apps/effort/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from apps.core.models import EmailUser
+from apps.tasks.models import Task
+
+
+class Effort(models.Model):
+    owner = models.ForeignKey(EmailUser, on_delete=models.CASCADE, related_name="effort_set")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="effort_set")
+
+    started = models.DateTimeField()
+    ended = models.DateTimeField()
+
+
+    class Meta:
+        indexes = [models.Index(fields=["-ended"])]
--- backend/threes/apps/tasks/models.py
+++ backend/threes/apps/tasks/models.py
@@ -5,4 +5,8 @@ from apps.core.models import EmailUser
 
 class Task(models.Model):
     owner = models.ForeignKey(EmailUser, on_delete=models.CASCADE, related_name="tasks")
+
     name = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+
+    created = models.DateTimeField(auto_now_add=True)
