0099fd4ab07bb46684f4ffa73d234ec4572c132f
Radek Stibora <rstibora@gmail.com>
2021-04-02 11:56:33 +0200

    Sketch the initial version of review periods

---
+++ backend/threes/apps/reviews/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+from apps.core.models import EmailUser
+
+
+class ReviewPeriodDuration(models.Model):
+    DAY = "D"
+    WEEK = "W"
+    MONTH = "M"
+    YEAR = "Y"
+    BASE_DURATION_CHOICES = [
+        (DAY, "Day"),
+        (WEEK, "Week"),
+        (MONTH, "Month"),
+        (YEAR, "Year")]
+
+    base_duration = models.CharField(max_length=1, choices=BASE_DURATION_CHOICES)
+    multiplier = models.IntegerField()
+
+    # Predefined durations should have no owner.
+    owner = models.ForeignKey(EmailUser, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=64)
+
+
+class ReviewPeriodConfiguration(models.Model):
+    owner = models.ForeignKey(EmailUser, on_delete=models.CASCADE,
+                              related_name="review_period_configurations")
+
+    name = models.CharField(max_length=64)
+    starts = models.DateTimeField()
+    inteval = models.ForeignKey(ReviewPeriodDuration, on_delete=models.PROTECT)
+
+
+class ReviewPeriod(models.Model):
+    owner = models.ForeignKey(EmailUser, on_delete=models.CASCADE, related_name="review_periods")
+    configuration = models.ForeignKey(ReviewPeriodConfiguration, on_delete=models.PROTECT)
+
+    starts = models.DateTimeField()
+    ends = models.DateTimeField()
