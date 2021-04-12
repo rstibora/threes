74fd7acf175e89e48c52a5e1da20ac970c5a2596
Radek Stibora <rstibora@gmail.com>
2021-04-12 20:48:47 +0200

    Use a bit of that syntax sugar

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -13,19 +13,18 @@ export default createStore({
     }
   },
   actions: {
-      fetchTasks(context) {
-        fetch("http://127.0.0.1:8000/api/tasks")
-        .then(response => response.json())
-        .then(json => {
-            let tasks = []
-            for (let task of json) {
-                tasks.push({
-                    "name": task["name"],
-                    "description": task["description"],
-                })
-            }
-            context.commit("updateTasks", {"tasks": tasks})
-        })
+      async fetchTasks(context) {
+        const response = await fetch("http://127.0.0.1:8000/api/tasks")
+        const json = await response.json()
+
+        let tasks = []
+        for (let task of json) {
+            tasks.push({
+                "name": task["name"],
+                "description": task["description"],
+            })
+        }
+        context.commit("updateTasks", {"tasks": tasks})
       }
   }
 })
