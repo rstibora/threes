4848383343a2083811da1e935be1e7d2401b9d3a
Radek Stibora <rstibora@gmail.com>
2021-04-19 20:44:47 +0200

    Bring back generic views

--- backend/threes/apps/tasks/views.py
+++ backend/threes/apps/tasks/views.py
@@ -1,31 +1,17 @@
+from django.db.models import query
 from django.http import Http404
 
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskList(APIView):
-    def get(self, request):
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
-class TaskDetail(APIView):
-    def get(self, request, pk):
-        try:
-            task = Task.objects.get(pk=pk)
-        except Task.DoesNotExist:
-            raise Http404
-        return Response(TaskSerializer(task).data, status=status.HTTP_200_OK)
+class TaskDetail(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
