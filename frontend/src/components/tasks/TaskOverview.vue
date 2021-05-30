d73123c17cc76c3da0cecefa7350c2646664bdcb
Radek Stibora <rstibora@gmail.com>
2021-05-30 21:13:48 +0200

    Improve TaskOverview view a bit

--- frontend/src/components/tasks/TaskOverview.vue
+++ frontend/src/components/tasks/TaskOverview.vue
@@ -1,10 +1,26 @@
 <template>
-    <h1>TASK</h1>
+    <h1>{{ task.name }}</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent, PropType } from "vue"
+import { mapState } from "vuex"
+
+import { Task } from "src/network/models/task"
 
 export default defineComponent({
+    props: {
+        taskId: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        ...mapState(["tasks"]),
+        task(): Task {
+            return this.tasks.get(this.taskId)
+        }
+
+    }
 })
 </script>
--- frontend/src/routing/router.ts
+++ frontend/src/routing/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHashHistory, RouteLocation } from "vue-router"
 
 import Dashboard from "src/components/Dashboard.vue"
 import TaskOverview from "src/components/tasks/TaskOverview.vue"
@@ -8,7 +8,9 @@ import Tasks from "src/components/Tasks.vue"
 const routes = [
     { path: "/dashboard", component: Dashboard, name: "dashboard", alias: "/" },
     { path: "/tasks", component: Tasks, name: "tasks" },
-    { path: "/tasks/:taskId", component: TaskOverview, name: "task" },
+    { path: "/tasks/:taskId", component: TaskOverview, name: "task",
+      props: (route: any) => ({ taskId: parseInt(route.params.taskId) }) },
+
 ]
 
 export default createRouter({
