5c41075c336d9d2d2da3e4e7118d4686f1b02619
Radek Stibora <rstibora@gmail.com>
2021-09-13 07:07:50 +0200

    Improve the test a bit

--- frontend/src/state/modules/tests/testModuleUtils.ts
+++ frontend/src/state/modules/tests/testModuleUtils.ts
@@ -5,8 +5,13 @@ import { updateOrDeleteInMap } from "src/state/modules/moduleUtils"
 
 @suite class EffortsModuleTests {
     @params({ stateItems: new Map([[1, "one"]]), items: new Map([[2, "two"]]), expected: new Map([[1, "one"], [2, "two"]])})
+    @params({ stateItems: new Map([[1, "one"], [2, "two"]]), items: new Map([[2, undefined]]), expected: new Map([[1, "one"]])})
     testUpdateOrDeleteInMap({ stateItems, items, expected} ) {
         updateOrDeleteInMap(stateItems, items)
+
         expect(stateItems.size).toBe(expected.size)
+        for (const [id, item] of items) {
+            expect(stateItems.get(id)).toBe(item)
+        }
     }
 }
