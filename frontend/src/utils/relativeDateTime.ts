daed902c397b0cb9c97e99bdebfa17b0559c9b73
Radek Stibora <rstibora@gmail.com>
2021-11-02 06:58:43 +0100

    Update npm packages

--- frontend/src/utils/relativeDateTime.ts
+++ frontend/src/utils/relativeDateTime.ts
@@ -2,7 +2,9 @@ import { DateTime, DurationObjectUnits } from "luxon"
 
 
 export function relativeDateTime(datetime: DateTime): string {
-    const units: (keyof DurationObjectUnits)[] = ["year", "month", "week", "day", "hour", "minute", "second"]
+    /** Produces string with human readable duration from the provided date. */
+    const units: (keyof DurationObjectUnits)[] = [
+        "years", "months", "weeks", "days", "hours", "minutes", "seconds"]
     const diff = datetime.diffNow().shiftTo(...units)
     const relativeFormatter = new Intl.RelativeTimeFormat('en', { numeric: "auto" });
     const unit = units.find((unit) => diff.get(unit) !== 0) || units[units.length - 1]
