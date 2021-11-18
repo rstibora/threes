407ca8d59a061ae07c7b2856ddcf890a1d6c426a
Radek Stibora <rstibora@gmail.com>
2021-11-18 19:07:11 +0100

    Add sockets and services and stuff

--- backend/server_setup/readme.md
+++ backend/server_setup/readme.md
@@ -1,6 +1,7 @@
-To setup production server, run these in order:
-1. TODO: setup_firewall as seen on digital ocean website
+To setup a fresh production server, run these in order:
+1. setup_firewall.sh
 2. install_packages.sh
 3. setup_postgres.sh
 4. setup_python.sh
 5. setup_data.sh
+6. setup_gunicorn_and_nginx.sh
---
+++ backend/server_setup/setup_gunicorn_and_nginx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Copying gunicorn socket and service units to /etc/systemd/system..."
+sudo cp ./gunicorn.service /etc/systemd/system/
+sudo cp ./gunicorn.socket /etc/systemd/system/
+
+echo "Starting the the socket..."
+sudo systemctl start gunicorn.socket
+sudo systemctl enable gunicorn.socket
+
+echo "Set environment variables:"
+# [Service]
+# Environment="SECRET=123"
+sudo systemctl edit gunicorn.service
+
+echo "Setting up nginx..."
+sudo cp ./threes /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/threes /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+sudo ufw allow 'Nginx Full'
