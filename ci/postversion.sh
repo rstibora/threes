b3f8c7eac83b2428ba99722afa331920454eeae4
Radek Stibora <rstibora@gmail.com>
2021-11-23 06:06:15 +0100

    Fix postversion script

--- ci/postversion.sh
+++ ci/postversion.sh
@@ -3,18 +3,19 @@
 git push && git push --tags
 
 export PIPENV_PIPFILE=../backend/Pipfile
+export DJANGO_DEBUG=true
 pipenv run python ../backend/threes/manage.py collectstatic
 
 rsync -e "ssh -i $HOME/.ssh/id_rsa" -rP ../backend/static_root threes@164.90.218.235:/home/threes/threes/backend
 
 echo "Logging into the production server..."
-ssh -i $HOME/.ssh/id_rsa threes@164.90.218.235
+ssh -i $HOME/.ssh/id_rsa threes@164.90.218.235 <<- EOF
+    cd /home/threes/threes
+    git pull
+    echo "Restarting gunicorn..."
+    sudo -u threes systemctl restart gunicorn
+    exit
+EOF 
 
-cd /home/threes/threes
-git pull
-
-echo "Restarting gunicorn..."
-sudo -u threes systemctl restart gunicorn
-exit
 
 rm -rf ./backend/static_root
